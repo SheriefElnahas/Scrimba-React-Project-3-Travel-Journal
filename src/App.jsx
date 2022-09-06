cf5c47937d4e11c2e8b3a6ad3bfb798e0c811fca
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-06 22:16:45 +0200

    make card component reusable ( data driven application )

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Card from './Card'
 
+import data from './data.js';
+
+
 function App() {
+  const cardElement = data.map(card => {
+    return <Card key={card.id} item={card} />
+  })
   return (
     <div className="app">
         <header>
 
             <p>My Travel Journal</p>
         </header>
-        < Card />
+       {cardElement}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,21 @@
 import './Card.css';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 
-function Card() {
+function Card(props) {
+    console.log(props);
     return (
         <div className="card">
-            <img className="card-img" src="https://source.unsplash.com/WLxQvbMyfas" />
+            <img className="card-img" src={props.item.imageUrl} />
             <div className="card-content">
                 <div className="card-location-details">
-                <p className="card-country"><FontAwesomeIcon className="icon" icon={faLocationDot} /> Japan</p>
-                <a href="#" className="view-country">View on Google Maps</a>
+                <p className="card-country"><FontAwesomeIcon className="icon" icon={faLocationDot} /> {props.item.location}</p>
+                <a href={props.item.googleMapsUrl} className="view-country">View on Google Maps</a>
                 </div>
-                <h1 className="card-location-name">Mount Fuji</h1>
-                <p className="card-date">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="card-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1 className="card-location-name">{props.item.title}</h1>
+                <p className="card-date">{props.item.date}</p>
+                <p className="card-text">{props.item.description}</p>
             </div>
         </div>
     )
