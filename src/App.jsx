c814c06a660f2ec65610e225e2e8fe7f9cb45878
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-06 21:36:10 +0200

    add header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+        <header>
+          <img src="#" alt="" />
+            <p>My Travel Journal</p>
+        </header>
+    </div>
+  )
+}
+
+export default App
